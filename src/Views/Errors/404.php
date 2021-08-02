83f936a9024743ca7f7a6a209e8083cf4930be1c
David Yell <david.yell@comparisontech.com>
2021-08-02 11:36:48 +0100

    Added a 404 page
    Fixed up how datastore results are asserted for being empty

---
+++ src/Views/Errors/404.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="/bootstrap/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="/bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+
+<div class="row mt-5">
+    <div class="col-md-12 text-center">
+        <h1>Oh noes!</h1>
+        <p>We're sorry, but that page does not exist!</p>
+        <p><a href="/" title="Return to home" class="btn btn-primary">Go back to the homepage</a></p>
+    </div>
+</div>
