29f5de9d88fdf5bccf008a1e705bde1f1c4edec9
David Yell <david.yell@comparisontech.com>
2021-08-02 11:35:50 +0100

    Fixed code quality issue

--- webroot/index.php
+++ webroot/index.php
@@ -5,4 +5,4 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals();
 
 $app = new \App\Application();
-$response = $app->dispatch($request);
+$app->dispatch($request);
