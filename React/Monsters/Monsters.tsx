62f713d483e57c74d97dad841507102c8ea4c53c
David Yell <david.yell@comparisontech.com>
2021-08-02 11:35:39 +0100

    Started the React+Typescript parts using Parcel

---
+++ React/Monsters/Monsters.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Monsters = () => {
+    return <h1>Monsters</h1>;
+}
---
+++ React/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+import { Monsters } from "./Monsters/Monsters"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Monsters/>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
